62c7927c39f653414e1d46d01752a73379f98889
Diego Sanchez Carcaba <dscarcaba@gmail.com>
2017-04-29 19:47:42 +0200

    creacion componente2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { Componente1Component } from './components/componente1/componente1.component';
+import { Componente2Component } from './components/componente2/componente2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Componente1Component
+    Componente1Component,
+    Componente2Component
   ],
   imports: [
     BrowserModule,
