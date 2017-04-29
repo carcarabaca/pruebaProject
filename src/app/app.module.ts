0175c528006751d15d0b4e07ed4026dbbbcf07bb
Diego Sanchez Carcaba <dscarcaba@gmail.com>
2017-04-29 19:20:01 +0200

    creo nuevo componente componente1
    Se ha creado un nuevo componente llamado componente1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Componente1Component } from './components/componente1/componente1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente1Component
   ],
   imports: [
     BrowserModule,
